ace0e0396a422363e231c9576d96a3028e4dd552
Arbusta <gabriel.munoz@arbusta.net>
2022-10-14 00:17:00 -0300

    Arrays - Objetos

--- Codigo/13 11-10/ejerciciosJS3.js
+++ Codigo/13 11-10/ejerciciosJS3.js
@@ -158,86 +158,47 @@ console.log(j)
 }
 cuentoElementos(num1)
 
-const diaDeLaSemana = (numeroDeDia) => {
+function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-if (numeroDeDia=== 1||numeroDeDia===7){
-  return "Es finde de semana"
-}
-else{
-  return "es dia laboral"
-}
   
 } 
-console.log (diaDeLaSemana(7))
 
 
-const empiezaConNueve =(n)=> {
+function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  n = n.toString();
-
-  return n.charAt(0)==9
+  
 }
 
-console.log(empiezaConNueve (9))
 
-const iguales = [1,1,1,2,1,1]
-const todosIguales=(arreglo)=> {
+function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí
-let contador = arreglo[0];
-for (let i=1;i<arreglo.length;i++){
-  if(contador!=arreglo[i])
-  return false
-}
-return true
+  //Escribe tu código aquí  
   
 } 
-console.log(todosIguales(iguales))
 
-const meses = ["Enero", "Noviembre","Diciembre","Abril", "Marzo"]
-const mesesDelAño=(array)=> {
+
+function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-  let nuevoArray= [];
-  for (let i = 0; i<array.length;i++){
-    if(array[i]=="Enero"||array[i]=="Marzo"||array[i]=="Noviembre")
-    nuevoArray.push(array[i])
-  }
-  if(nuevoArray.length<3){
-    return "no se encontraron los meses pedidos"
-  }
-else{
-  return nuevoArray
 }
-}
-console.log(mesesDelAño(meses))
 
-const MaCien= [90,100,105,200,88]
-const mayorACien=(array)=> {
+
+function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  let nuevoArray= [];
-
-  for (let i = 0;i<array.length;i++){
-    if(array[i]>100){
-      nuevoArray.push(array[i]) 
-    }
-  }
-  return nuevoArray
 }
-console.log (mayorACien(MaCien))
 
 
-const breakStatement= (numero) =>{
+function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array. 
   //Devolver el array
@@ -245,36 +206,14 @@ const breakStatement= (numero) =>{
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  let nuevoArray= [];
-  for (i=0;i<10;i++){
-    numero+=2;
-    nuevoArray.push(numero);
-    if(numero===i){
-      console.log ( "Se interrumpio la ejecucion")
-      break;
-    }
-    console.log (nuevoArray)
-  }
 }
-// num(-2)= -2  0 2
-// i =      0  1  2  3
-breakStatement(-2)
 
-const continueStatement= (numero)=> {
+
+function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
   //Devolver el array
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  let nuevoArray= [];
-  for (let i=0;i<10;i++){
-    numero+=2;
-    if (i===5){
-      continue;
-    }
-    nuevoArray.push(numero)
-  }
-  return nuevoArray
 }
-console.log(continueStatement(1))
---
+++ Codigo/14 13-10/objetos.js
@@ -0,0 +1,11 @@
+
+const heroe = {
+    // clave  valor
+    nombre: "Bruno Diaz",
+    ciudad: "Gotica",
+    batimovil: true,
+    liga: ["Superman", "Wonder Woman"]
+}
+
+// notacion de puntos
+console.log(`el Heroe es ${heroe.nombre} y vive en ${heroe.ciudad}`)
