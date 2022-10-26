3ba9685839d40fb64d02bc3bd9e3aad087b77cd7
Arbusta <gabriel.munoz@arbusta.net>
2022-10-25 23:02:26 -0300

    react

---
+++ Codigo/16 27-10/mi-primer-app/src/Boton.jsx
@@ -0,0 +1,17 @@
+/* 
+
+function Boton (props) {
+return (
+    <button>{props.numero}</button>
+)
+
+} */
+
+function Boton ({numero}) {
+    return (
+        <button>{numero}</button>
+    )
+    
+    }
+
+export default Boton;
---
+++ Codigo/16 27-10/mi-primer-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Boton from './Boton';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+    <Boton numero = {1} />
+    <Boton numero = {2} />
+    <Boton numero = {3} />
+    <Boton numero = {4} />
+    <Boton />
+  </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
